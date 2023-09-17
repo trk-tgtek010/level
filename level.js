6ff0b69988ba36b70d6aecafe014703fedbdfb54
trk-tgtek010 <145299625+trk-tgtek010@users.noreply.github.com>
2023-09-18 00:08:35 +0100

    Add files via upload

---
+++ level.js
@@ -0,0 +1,31 @@
+$(function () {
+  $("#checkin").datepicker();
+  $("#checkout").datepicker();
+  $('.level-slider').slick({
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+  });
+
+  function videoToggle() {
+    video = $('.level-video').get(0)
+    if (video.paused) {
+      video.play();
+      $('.video-control-play').hide();
+      $('.video-control-pause').show();
+    } else {
+      video.pause();
+      $('.video-control-play').show();
+      $('.video-control-pause').hide();
+    }
+  }
+
+  $('.video-control-play').click(function () {
+    videoToggle();
+  })
+  $('.video-control-pause').click(function () {
+    videoToggle();
+  })
+
+});
